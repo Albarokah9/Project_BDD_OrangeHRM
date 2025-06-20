d8f5047b66869316521bbefe558fed84d571b0c8
Albar <albareizy942@gmail.com>
2025-06-20 14:16:23 +0700

    add base url https://opensource-demo.orangehrmlive.com/

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.feature", // Path ke file .feature kamu
     supportFile: "cypress/support/e2e.js",   // File support utama
+    baseUrl: "https://opensource-demo.orangehrmlive.com/",
     setupNodeEvents: async (on, config) => {
       await addCucumberPreprocessorPlugin(on, config, {
         stepDefinitions: ["cypress/support/Step_Definitions"], // <== path ke step
