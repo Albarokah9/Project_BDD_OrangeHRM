f193f59c0737e6e764511216c3ae326db6417731
Albar <albareizy942@gmail.com>
2025-06-20 22:16:01 +0700

    add allure report

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -10,6 +10,8 @@ module.exports = defineConfig({
     supportFile: "cypress/support/e2e.js",   // File support utama
     baseUrl: "https://opensource-demo.orangehrmlive.com/",
     setupNodeEvents: async (on, config) => {
+      // Tambahkan plugin Allure
+      allureWriter(on, config);
       await addCucumberPreprocessorPlugin(on, config, {
         stepDefinitions: ["cypress/step_definitions/**/*.js"], // <== path ke step
       });
